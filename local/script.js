21e0e4121c3ff02505f868b02b76f57a55a209d4
ivonebelli <ivonebellicontreras@gmail.com>
2025-05-13 16:03:36 -0300

    Delete local directory

--- local/script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener(DOMContentLoaded, function() {
-    document.getElementById(titulo).innerText = Nuevo título;
-    document.getElementById(descripcion).innerText = Texto actualizado.;
-});
