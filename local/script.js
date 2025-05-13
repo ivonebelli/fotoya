05a7eb00a8c5b10a9e78711de33129a9e80ca66a
ivonebelli <ivonebellicontreras@gmail.com>
2025-05-13 15:44:15 -0300

    Add files via upload

---
+++ local/script.js
@@ -0,0 +1,4 @@
+document.addEventListener(DOMContentLoaded, function() {
+    document.getElementById(titulo).innerText = Nuevo título;
+    document.getElementById(descripcion).innerText = Texto actualizado.;
+});
